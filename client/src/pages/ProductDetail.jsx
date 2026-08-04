a9461aaf4016a148829f1c7035d2b61363ba4146
Maniha-dev <nasirrazaptcl@gmail.com>
2026-08-04 22:35:30 +0500

    Completed Productdetail page

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useAppContext } from '../context/AppContext'
 import { useParams, Link } from "react-router-dom"
 import { assets } from '../assets/assets' // Fixed: Named import
+import ProductCard from '../components/ProductCard'
 
 const ProductDetail = () => {
     // Check if your context uses addToCart or addToChart
@@ -108,9 +109,16 @@ const ProductDetail = () => {
             {/* --------- related products ------------ */}
             <div className="flex flex-col items-center mt-20">
                 <div className="flex flex-col items-center w-max">
-                    <p>Related Products</p>
+                    <p className="text-3xl font-medium">Related Products</p>
                        <div className="w-20 h-0.5 bg-primary rounded-full mt-2"></div>
                 </div>
+                
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6 w-full">
+                    {relatedProducts.filter((product)=>product.inStock).map ((product, index)=>(
+                        <ProductCard key={index} product={product}/>
+                    ))}
+                </div>
+                <button onClick={() => {navigate('/products'); scrollTo(0,0)}} className="mx-auto cursor-pointer px-12 my-16 py-2.5 border rounded text-primary hover:bg-primary/10 transition">See more</button>
             </div>
         </div>
     ) : null
