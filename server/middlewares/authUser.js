929aabdf98c60c762419b13ac36605d16aaeaffe
Maniha-dev <nasirrazaptcl@gmail.com>
2026-08-14 23:36:15 +0500

    Backend Added

--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -1,25 +1,26 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken"
 
 const authUser = async (req, res, next) => {
     const { token } = req.cookies;
 
     if (!token) {
-        return res.json({ success: false, message: 'Not Authorized' });
+        return res.json({ success: false, message: 'Not Authorized' })
     }
 
     try {
-        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
 
         if (tokenDecode.id) {
-            req.userId = tokenDecode.id; // Directly attach to req object
+            req.body.userId = tokenDecode.id
         } else {
-            return res.json({ success: false, message: 'Not Authorized' });
+            return res.json({ success: false, message: 'Not Authorized' })
         }
 
         next();
-    } catch (error) {
-        res.json({ success: false, message: error.message });
-    }
-};
 
-export default authUser;
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export default authUser
--- server/models/product.js
+++ server/models/product.js
@@ -6,7 +6,7 @@ const productSchema = new mongoose.Schema({
     price: { type: Number, required: true },
     offerPrice: { type: Number, required: true },
     image: { type: Array, required: true },
-    category: { type: String, required: true },   // ⚠️ ye String hi rakhna, Array nahi (video mein galti se Array likha tha, baad me correct kiya)
+    category: { type: String, required: true },
     inStock: { type: Boolean, default: true },
 }, { timestamps: true })
 
